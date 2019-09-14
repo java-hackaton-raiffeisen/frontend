099b8190b8d94b70f0917385c088b7911d672d31
a.lee <a.lee@ktsstudio.ru>
2019-09-14 15:41:01 +0300

    init

---
+++ src/components/ClientsTableSample.vue
@@ -0,0 +1,152 @@
+<template>
+  <div>
+    <modal-box
+      :is-active="isModalActive"
+      :trash-object-name="trashObjectName"
+      @confirm="trashConfirm"
+      @cancel="trashCancel"
+    />
+    <b-table
+      :checkable="checkable"
+      :loading="isLoading"
+      :paginated="paginated"
+      :per-page="perPage"
+      :striped="true"
+      :hoverable="true"
+      default-sort="name"
+      :data="clients"
+    >
+      <template slot-scope="props">
+        <b-table-column class="has-no-head-mobile is-image-cell">
+          <div class="image">
+            <img :src="props.row.file" class="is-rounded" />
+          </div>
+        </b-table-column>
+        <b-table-column label="Name" field="name" sortable>
+          {{ props.row.fullName }}
+        </b-table-column>
+
+        <b-table-column label="Created">
+          <small
+            class="has-text-grey is-abbr-like"
+            :title="props.row.created"
+            >{{ props.row.created }}</small
+          >
+        </b-table-column>
+        <b-table-column custom-key="actions" class="is-actions-cell">
+          <div class="buttons is-right">
+            <button class="button is-small is-primary" type="button">
+              <b-icon icon="eye" custom-size="default" />
+            </button>
+            <button
+              class="button is-small is-danger"
+              type="button"
+              @click.prevent="trashModal(props.row)"
+            >
+              <b-icon icon="trash-can" custom-size="default" />
+            </button>
+          </div>
+        </b-table-column>
+      </template>
+
+      <section class="section" slot="empty">
+        <div class="content has-text-grey has-text-centered">
+          <template v-if="isLoading">
+            <p>
+              <b-icon icon="dots-horizontal" size="is-large" />
+            </p>
+            <p>Загрузка...</p>
+          </template>
+          <template v-else>
+            <p>
+              <b-icon icon="emoticon-sad" size="is-large" />
+            </p>
+            <p>Пока нет клиентов&hellip;</p>
+          </template>
+        </div>
+      </section>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import ModalBox from "@/components/ModalBox";
+
+export default {
+  name: "ClientsTableSample",
+  components: { ModalBox },
+  props: {
+    dataUrl: {
+      type: String,
+      default: null
+    },
+    checkable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      isModalActive: false,
+      trashObject: null,
+      clients: [
+        {
+          fullName: "Adnrew Lee"
+        }
+      ],
+      isLoading: false,
+      paginated: false,
+      perPage: 10
+    };
+  },
+  computed: {
+    trashObjectName() {
+      if (this.trashObject) {
+        return this.trashObject.name;
+      }
+
+      return null;
+    }
+  },
+  // mounted () {
+  //   if (this.dataUrl) {
+  //     this.isLoading = true
+  //     axios
+  //       .get(this.dataUrl)
+  //       .then(r => {
+  //         this.isLoading = false
+  //         if (r.data && r.data.data) {
+  //           if (r.data.data.length > this.perPage) {
+  //             this.paginated = true
+  //           }
+  //           this.clients = r.data.data
+  //         }
+  //       })
+  //       .catch(e => {
+  //         this.isLoading = false
+  //         this.$buefy.toast.open({
+  //           message: `Error: ${e.message}`,
+  //           type: 'is-danger'
+  //         })
+  //       })
+  //   }
+  // },
+  methods: {
+    trashModal(trashObject) {
+      this.trashObject = trashObject;
+      this.isModalActive = true;
+    },
+    trashConfirm() {
+      this.isModalActive = false;
+      this.$buefy.snackbar.open({
+        message: "Confirmed",
+        queue: false
+      });
+    },
+    trashCancel() {
+      this.isModalActive = false;
+    }
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section">
+    <title-bar>
+      Список клиентов
+    </title-bar>
+
+    <card-component class="has-table">
+      <clients-table-sample />
+    </card-component>
+  </section>
+</template>
+
+<script>
+import Notification from "@/components/Notification";
+import ClientsTableSample from "@/components/ClientsTableSample";
+import CardComponent from "@/components/CardComponent";
+import TitleBar from "@/components/TitleBar";
+export default {
+  name: "clients",
+  components: { TitleBar, CardComponent, ClientsTableSample, Notification }
+};
+</script>
